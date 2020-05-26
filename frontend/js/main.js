07a8464fe89f2b8f3b86d652a560910fd8112a58
bro-rukman <rukmanaendang99@gmail.com>
2020-05-26 19:57:54 +0700

    modified

---
+++ frontend/js/main.js
@@ -0,0 +1,10 @@
+var a = function (width, height) {
+    return width * height;
+}
+var total = a(12, 2);
+console.log(total);
+var multi = total * 2;
+console.log(multi);
+var index = prompt("input : ");
+done = multi - index;
+console.log(done);
