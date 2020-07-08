bdbda81de449d5cba0403c665a1cd1cff0a17c84
bro-rukman <rukmanaendang99@gmail.com>
2020-07-08 21:49:56 +0700

    done modified

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,3 +8,24 @@ console.log(multi);
 var index = prompt("input : ");
 done = multi - index;
 console.log(done);
+document.write(done);
+var random = Math.random();
+// document.write("<br>" + random);
+if (random < 0.50) {
+    document.write("<br>kalah")
+} else {
+    document.write("<br>Menang")
+}
+var hotel = newObject();
+hotel.hot = 'hotels',
+    hotel.room = 40,
+    hotel.booked = 23,
+    hotel.gym = true,
+    hotel.roomType = ['twin', 'double', 'suite']
+hotel.checkAvability = function () {
+    return this.room - this.booked;
+}
+
+var available = document.getElementById('rooms');
+available.textContent = hotel.checkAvability();
+document.write(available);
